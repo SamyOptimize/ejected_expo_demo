3d4ab9c49c2ce1efa5720489164a8533900f25b9
SamyOptimize <samy@optimizekw.com>
2020-03-09 17:18:09 +0300

    First commit

---
+++ android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
@@ -0,0 +1,8 @@
+package host.exp.exponent.generated;
+
+// This file is auto-generated. Please don't rename!
+public class DetachBuildConstants {
+
+  public static final String DEVELOPMENT_URL = "expccdca49a6a8942f996a92d490000556d://192.168.8.198:19004";
+
+}
